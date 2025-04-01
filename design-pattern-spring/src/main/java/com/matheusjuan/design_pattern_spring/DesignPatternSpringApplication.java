b8712b15d139e0a700b7366ec247f098baf2d810
Matheus Ferreira <matheus.ferreira@bandtec.com.br>
2025-04-01 19:11:07 -0300

    feat: add spring for design pattern

---
+++ design-pattern-spring/src/main/java/com/matheusjuan/design_pattern_spring/DesignPatternSpringApplication.java
@@ -0,0 +1,16 @@
+package com.matheusjuan.design_pattern_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author matheusjuan1
+ */
+@SpringBootApplication
+public class DesignPatternSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternSpringApplication.class, args);
+	}
+
+}
---
+++ design-pattern-spring/src/test/java/com/matheusjuan/design_pattern_spring/DesignPatternSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusjuan.design_pattern_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
